8c530271ec1a923c8497459b814973c9958b1fe5
phuoc <phuocvan27102002@gmail.com>
2023-12-29 23:09:47 +0700

    create fact table and schema test

--- models/dw/dim_store.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    s.StoreID,
-    s.StoreName,
-    e.territory_country,
-    e.territory_group
-FROM {{ source('nds', 'store')}} s 
-LEFT JOIN {{ ref('dim_employee')}} e
-    ON s.SalesPersonID = e.employee_code
